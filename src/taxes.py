372607473c456aba5d8af104d13eb93334e6e5e6
Irina Chuvashova <irishkachuv@gmail.com>
2026-09-17 17:25:06 +0300

    add new func and tests

--- src/taxes.py
+++ src/taxes.py
@@ -14,3 +14,15 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
 
     return taxed_prices
 
+
+def calculate_tax(price: float, tax_rate: float) -> float:
+
+    if price <= 0:
+        raise ValueError('Неверная цена')
+
+    if tax_rate <= 0 or tax_rate > 100:
+        raise ValueError('Неверная процентная ставка')
+
+    result = price * tax_rate / 100
+    return result + price
+
--- tests/test_taxes.py
+++ tests/test_taxes.py
@@ -1,5 +1,5 @@
 import pytest
-from src.taxes import calculate_taxes
+from src.taxes import calculate_taxes, calculate_tax
 
 
 @pytest.fixture
@@ -24,3 +24,19 @@ def test_calculate_taxes_invalid_prices():
     with pytest.raises(ValueError):
         calculate_taxes([0, -1], tax_rate=10)
 
+
+@pytest.mark.parametrize('price, tax_rate, expected', [
+    (100, 10, 110),
+    (50, 5, 52.5)])
+def test_calculate_tax(price, tax_rate, expected):
+    assert calculate_tax(price, tax_rate) == expected
+
+
+def test_calculate_tax_invalid_price():
+    with pytest.raises(ValueError):
+        calculate_tax(-1, tax_rate=10)
+
+
+def test_calculate_tax_invalid_tax_rate():
+    with pytest.raises(ValueError):
+        calculate_tax(100, tax_rate=-1)
